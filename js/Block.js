d7df37312c99315712589ab117c6e02786cb1f64
ashvikrachamalla <68549074+ashvikrachamalla@users.noreply.github.com>
2020-09-05 11:43:15 +0530

    Add files via upload

--- js/Block.js
+++ js/Block.js
@@ -11,15 +11,25 @@ class Block{
         this.width = width;
         this.height = height;
         World.add(world, this.body);
+        this.visiblity = 100;
+
       }
       display(){
-        var angle = this.body.angle;
-        var pos= this.body.position;
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        rectMode(CENTER);
-        rect(0,0,this.width, this.height);
-        pop();
+        if (this.body.speed < 3) {
+            var angle = this.body.angle;
+            var pos= this.body.position;
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0,0,this.width, this.height);
+            pop();
+        }else {
+          World.remove (world, this.body);
+          this.visiblity -= 2;
+        } 
+
+        
+      
       }
 }
--- js/SlingShot.js
+++ js/SlingShot.js
@@ -11,6 +11,10 @@ class Slingshot{
         World.add(world, this.sling);
 
     }
+    attach (body){
+        this.sling.bodyA = body;
+        
+    }
     fly(){
         this.sling.bodyA =null;
     }
